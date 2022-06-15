54e26fd11edc6611b97c7065e881deba2e43388d
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2022-06-15 10:38:53 +0200

    adding models to repo

---
+++ ImageDetector.py
@@ -0,0 +1,10 @@
+
+
+import numpy as np 
+import cv2
+
+
+image_path = 'tempimages/000001.jpg'
+prototxt_path = 'models/MobileNetSSD_deploy.prototxt'
+model_path = 'models/MobileNetSSD_deploy.caffemodel'
+
