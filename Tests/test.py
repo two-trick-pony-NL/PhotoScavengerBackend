9217ada0a353ce1383f61dafc41f94b01650ff4b
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2022-08-02 16:11:34 +0200

    added test for new assignment API

---
+++ Tests/test.py
@@ -0,0 +1,26 @@
+import requests
+import random
+
+"""
+This test runs against the localhost server and tries permutations of the possible score and the assignment + emoji the user can get back
+If there's a error in the json response it might result a failure for the user.
+
+By running a great number of permutations we have assurance that the API works
+
+"""
+
+
+URL = "http://localhost:8000/v2/newassignment/"
+
+numberoftries = 3000
+
+for i in range (numberoftries):
+    j = random.randint(0, 12000)
+    j = str(j)
+    r = requests.get(url = URL+j)
+    data = r.json()
+    i = str(i)
+    numberoftries = str(numberoftries)
+    print("\n##########     " + i + "  out of  " + numberoftries + "     ##########\n")
+    print("Tried score:   " + j)
+    print(data)
