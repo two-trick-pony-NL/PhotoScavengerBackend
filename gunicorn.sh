2bff0ceb6db8e937acbc4cf241072849314fd463
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2022-08-21 18:07:18 +0200

    changed workers

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#uvicorn main:app --host=0.0.0.0 --port 80
-gunicorn main:app -w 4 --threads 2 -b 0.0.0.0:80
+uvicorn main:app --host=0.0.0.0 --port 80 --workers 2
+#gunicorn main:app -w 4 --threads 2 -b 0.0.0.0:80
