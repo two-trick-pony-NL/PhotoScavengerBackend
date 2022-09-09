13e61f2780d278d6bfb7983f82ff6d9ff6bf8763
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2022-09-09 15:50:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def daily_analytics_update():
 # This schedular runs every hour making a snapshot of the stats.json 
 # file and stores in in the longermdata file
 scheduler = BackgroundScheduler()
-scheduler.add_job(daily_analytics_update, 'interval', hours=1)
+scheduler.add_job(daily_analytics_update, 'interval', hours=48)
 scheduler.start()
 
 daily_analytics_update()
